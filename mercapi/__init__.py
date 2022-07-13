0e45d1b585c74d49e648979971c0efa7d7290fd9
take-kun <109226194+take-kun@users.noreply.github.com>
2022-07-13 23:09:17 +0200

    Signing requests with JWT. Sample implementation of search request.

---
+++ mercapi/__init__.py
@@ -0,0 +1,1 @@
+from .mercapi import Mercapi
---
+++ mercapi/mercapi.py
@@ -0,0 +1,60 @@
+import random
+import time
+import uuid
+
+import httpx
+from ecdsa import SigningKey, NIST256p
+from httpx import Request
+from jose import jws
+from jose.backends.ecdsa_backend import ECDSAECKey
+from jose.constants import ALGORITHMS
+
+
+class Mercapi:
+    _api_endpoint = 'https://api.mercari.jp/v2/entities:search'
+    _user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0'
+
+    def __init__(self):
+        self._uuid = str(uuid.UUID(int=random.getrandbits(128)))
+        self._key = SigningKey.generate(NIST256p)
+        self._client = httpx.AsyncClient()
+
+    def _prepare_request(self, request: Request) -> Request:
+        payload = {
+            'iat': int(time.time()),
+            'jti': str(uuid.UUID(int=random.getrandbits(128))),
+            'htu': str(request.url),
+            'htm': request.method,
+            'uuid': self._uuid,
+        }
+
+        ec_key = ECDSAECKey(self._key, ALGORITHMS.ES256)
+        headers = {
+            'typ': 'dpop+jwt',
+            'alg': 'ES256',
+            'jwk': {k: ec_key.to_dict()[k] for k in ['crv', 'kty', 'x', 'y']},
+        }
+        jwt = jws.sign(payload, self._key, headers, ALGORITHMS.ES256)
+
+        request.headers['DPoP'] = jwt
+
+        return request
+
+    async def search(self, query: str) -> dict:
+        res = await self._client.send(self._search(query))
+        return res.json()
+
+    def _search(self, query: str) -> Request:
+        data = {"userId": "", "pageSize": 120, "pageToken": "", "searchSessionId": uuid.uuid4().hex,
+                "indexRouting": "INDEX_ROUTING_UNSPECIFIED", "thumbnailTypes": [],
+                "searchCondition": {"keyword": query, "excludeKeyword": "", "sort": "SORT_SCORE",
+                                    "order": "ORDER_DESC", "status": [], "sizeId": [], "categoryId": [], "brandId": [],
+                                    "sellerId": [], "priceMin": 0, "priceMax": 0, "itemConditionId": [],
+                                    "shippingPayerId": [], "shippingFromArea": [], "shippingMethod": [], "colorId": [],
+                                    "hasCoupon": False, "attributes": [], "itemTypes": [], "skuIds": []},
+                "defaultDatasets": [], "serviceFrom": "suruga"}
+        req = Request('POST', self._api_endpoint,
+                      json=data,
+                      headers={'User-Agent': self._user_agent, 'X-Platform': 'web'}
+                      )
+        return self._prepare_request(req)
