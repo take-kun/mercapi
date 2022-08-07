30b26d838b35aeaa566b8d660e2c7573a16ff80e
take-kun <109226194+take-kun@users.noreply.github.com>
2022-08-07 15:12:24 +0200

    docs: Facet endpoints responses

--- example.py
+++ example.py
@@ -1,12 +1,19 @@
 import asyncio
+import logging
 
 from mercapi import Mercapi
+
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
 
 
 async def main():
     mercapi = Mercapi()
     results = await mercapi.search('sharpnel')
     print(results.items[0].name, results.items[0].price)
+    item = await mercapi.item('m94786104879')
+    print(item.name, item.price, item.status)
 
 
 if __name__ == '__main__':
