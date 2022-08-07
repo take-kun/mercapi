ff28662840b06e0412e1a3819c8cbab1144bd86a
take-kun <109226194+take-kun@users.noreply.github.com>
2022-08-08 01:07:29 +0200

    tests: Tests for search with filters. Added request throttling.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,14 @@
+import pytest
 from vcr import VCR
+
+from mercapi import mercapi
 
 my_vcr = VCR(
     cassette_library_dir='cassettes',
     path_transformer=VCR.ensure_suffix('.yml'),
 )
+
+
+@pytest.fixture(scope='function')
+def m():
+    return mercapi.Mercapi()
--- tests/test_search.py
+++ tests/test_search.py
@@ -1,15 +1,11 @@
 import pytest
 
-from mercapi import mercapi
-from tests import my_vcr
-
-
-m = mercapi.Mercapi()
+from conftest import my_vcr
 
 
 @pytest.mark.asyncio
 @my_vcr.use_cassette()
-async def test_simple_search_query():
+async def test_simple_search_query(m):
     res = await m.search('sharpnel')
     assert len(res.items) == 106
 
@@ -17,3 +13,38 @@ async def test_simple_search_query():
     assert item.name == '【廃盤】DJ SHARPNEL シャープネルサウンドコレクション壱'
     assert item.price == 9444
     assert item.status == 'ITEM_STATUS_ON_SALE'
+
+
+@pytest.mark.asyncio
+@my_vcr.use_cassette()
+async def test_search_with_item_condition_filter(m):
+    res = await m.search('sharpnel', item_conditions=[2])
+    assert [i.item_condition_id == 2 for i in res.items]
+
+
+@pytest.mark.asyncio
+@my_vcr.use_cassette()
+async def test_search_with_shipping_payer_filter(m):
+    res = await m.search('sharpnel', shipping_payer=[2])
+    assert [i.shipping_payer_id == 2 for i in res.items]
+
+
+@pytest.mark.asyncio
+@my_vcr.use_cassette()
+async def test_search_with_min_price_filter(m):
+    res = await m.search('sharpnel', price_min=5000)
+    assert [i.price >= 5000 for i in res.items]
+
+
+@pytest.mark.asyncio
+@my_vcr.use_cassette()
+async def test_search_with_max_price_filter(m):
+    res = await m.search('sharpnel', price_max=5000)
+    assert [i.price <= 5000 for i in res.items]
+
+
+@pytest.mark.asyncio
+@my_vcr.use_cassette()
+async def test_search_with_min_price_and_max_price_filter(m):
+    res = await m.search('sharpnel', price_min=5000, price_max=10000)
+    assert [5000 <= i.price <= 10000 for i in res.items]
