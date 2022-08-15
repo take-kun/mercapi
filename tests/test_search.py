6d4a0578af25cd5bd95cc8cb5ff9a01b13753d75
take-kun <109226194+take-kun@users.noreply.github.com>
2022-08-16 00:47:08 +0200

    tests: Fetching full item from search result

--- tests/test_search.py
+++ tests/test_search.py
@@ -12,7 +12,6 @@ async def test_simple_search_query(m):
     item = res.items[0]
     assert item.name == '【廃盤】DJ SHARPNEL シャープネルサウンドコレクション壱'
     assert item.price == 9444
-    assert item.status == 'ITEM_STATUS_ON_SALE'
 
     assert res.meta.num_found == 111
 
@@ -50,3 +49,13 @@ async def test_search_with_max_price_filter(m):
 async def test_search_with_min_price_and_max_price_filter(m):
     res = await m.search('sharpnel', price_min=5000, price_max=10000)
     assert [5000 <= i.price <= 10000 for i in res.items]
+
+
+@pytest.mark.asyncio
+@my_vcr.use_cassette()
+async def test_search_fetch_full_item_from_result(m):
+    res = await m.search('sharpnel')
+    item = res.items[0]
+
+    full_item = await item.full_item()
+    assert full_item.id_ == 'm29475719206'
